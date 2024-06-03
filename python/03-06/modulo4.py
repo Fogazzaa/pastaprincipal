8cf6d78f8292c88d57972974dffeb18408d0e659
fogazza <vinicius.cintra2801@gmail.com>
2024-06-03 16:15:28 -0300

    ultimo commit

--- python/03-06/modulo4.py
+++ python/03-06/modulo4.py
@@ -26,13 +26,52 @@ def somar2(n1, n2):
 def somar3(n1, n2):
     return n1 + n2
 
-print("")
-numero1 = float(input('digite um número: '))
-print("")
-numero2 = float(input('digite um número: '))
+# print("")
+# numero1 = float(input('digite um número: '))
+# print("")
+# numero2 = float(input('digite um número: '))
 
+# print("")
+# # chamada da função
+# print(somar3(numero1, numero2))
+# print("")
+
+# chamada da função por argumentos
 print("")
-print(somar3(numero1, numero2))
+print(somar3(n2 = 12, n1 = 5))
+print("")
+
+def somar4(n1 = 0, n2 = 0):
+    return n1 +n2
+
+# print(somar4(50,25))
+# print("")
+# print(somar4())
+# print("")
+
+def somar5(n1, n2 = 0):
+    return n1 + n2
+
+print(somar5(50,25)) # 75
+print("")
+# print(somar5()) # erro
+# print("")
+print(somar5(10))
+print("")
+print(somar5(n2 = 51, n1 = 12))
+print("")
+
+def somar6(n1, n2):
+    if n1 == 1:
+        return 1
+
+print(somar6(1,3)) # true
+print("")
+print(somar6(13,3)) # none
+print("")
+print(somar6(1,2) + 10) # mostra 11
+print("")
+print(somar6(1,2) + 10) # mostra 'type' suportado 'str'
 print("")
 
 time.sleep(2)
