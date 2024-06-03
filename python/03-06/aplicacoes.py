cbe8807b7c9e8b38720ce6d2e0c2c85e51e94553
fogazza <vinicius.cintra2801@gmail.com>
2024-06-03 11:33:02 -0300

    commit

--- python/03-06/aplicacoes.py
+++ python/03-06/aplicacoes.py
@@ -14,4 +14,43 @@ print("")
 print('maoir número: ', maiornumero)
 print("")
 
+# Exemplo 2
+
+mylist = [14, 4, 11, 5, 6 , 9, 7, 19, 12, 999999]
+maior = mylist[0]
+for i in mylist:
+    if i > maior:
+            maior = i
+
+print('maior da lista 2: ', maior)
+print("")
+
+# Exemplo 3
+
+ultimalista = mylist[:]
+mel = ultimalista[0]
+for i in ultimalista[1:]:
+    if i > mel:
+        mel = i
+
+print("maior valor da 3: ", mel)
+print("")
+
+# Encontrar a localização de um determinado elemento dentro de uma lista
+
+frutas = ['abacaxi', 'maça', 'pera', 'mamao', 'uva', 'melancia']
+elemento = 'melancia'
+achado = False
+
+for i in range(len(frutas)):
+    achado = frutas[i] == elemento
+    if achado == True:
+        break
+
+if achado == True: 
+    print('elemento encontrado no indice: ', i)
+else:
+    print('não encontrado')
+print("")
+
 time.sleep(1)
