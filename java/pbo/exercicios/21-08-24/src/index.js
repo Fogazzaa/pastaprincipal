791b213c03ef4c6a7ee78bf472ae1b53917bf9d9
fogazza <vinicius.cintra2801@gmail.com>
2024-08-28 14:10:52 -0300

    28-08-24

--- java/pbo/exercicios/21-08-24/src/index.js
+++ java/pbo/exercicios/21-08-24/src/index.js
@@ -1,29 +1,63 @@
+//Importa o modulo Express
+const express = require("express");
 
-const express = require("express"); // importando o módulo express
-
+//Define uma classe para organizar a logica da aplicação
 class AppController {
-  // define uma classe para organizar a lógica da aplicação
-
   constructor() {
-    this.express = express(); // cria uma nova instância dentro da classe
-    this.middlewares(); // chama o método middlewares para configurar os middlewares
-    this.routes(); //chama o método routes para definir as rotas da API
+    //Cria uma nova instancia do Express dentro da classe
+    this.express = express();
+    //Chama o metodo middlewares para configurar os middlewares
+    this.middlewares();
+    //Chama o metodo routes para definir as rotas da Api
+    this.routes();
   }
   middlewares() {
-    this.express.use(express.json()); // permitir que a aplicação receba dados em formato JSON nas requisições
+    //Permitir que a aplicação receba dados em formato JSON nas requisições
+    this.express.use(express.json());
   }
 
-  routes() { // define as rotas da nossa API
-    this.express.get("/health/", (req, res) => { // define uma rota 'GET' para o caminho '/health/'
-      res.send({ 
-        nome : "vinicius",
-        idade : "16",
-        CPF : "44595804890"
-       }); // essa rota é usada para verificar se a API está OK
-    }); 
+  //Define as rotas da nossa API
+  routes() {
+    const users = [];
+
+    this.express.post("/users", (req, res) => {
+      const { id, nome, email, senha } = req.body;
+      users.push({ id, nome, email, senha });
+      res.status(200).send({ message: "Usuario cadastrado com sucesso" });
+    });
+
+    this.express.post("/auth", (req, res) => {
+      const {email, senha } = req.body;
+      const login = users.find((login) => login.email == email && login.senha == senha);
+      if(login) {
+        res.status(200).send({ message: "Autenticaçãobem sucedida" });
+      }
+      else{
+        res.status(404).send({ message: "Email ou senha incorreta" });
+      }
+
+
+    });
+
+
+
+    this.express.get("/users/:id", (req, res) => {
+      const { id } = req.params;
+      const user = users.find((user) => user.id == id);
+      if(user){
+        res.status(200).send(user)
+      }
+      else{
+        res.status(400).send({message: "Usuario não encontrado"})
+      }
+    });
+
+    //Define uma rota GET para o caminho health
+    this.express.get("/health/", (req, res) => {
+      res.send({ status: "OK" });
+    }); //Essa rota é usada para verificar se a Api esta ok
   }
 }
 
-/* exportando a instância de express configurada, 
-para que seja acessada em outros arquivos */
-module.exports = new AppController().express; 
+//Exportando a instancia de Express configurada, para que seja acessada em outros arquivos
+module.exports = new AppController().express;
--- java/pbo/exercicios/21-08-24/src/server.js
+++ java/pbo/exercicios/21-08-24/src/server.js
@@ -2,4 +2,4 @@
 
 const app = require('./index'); // importa a instância do express gofigurada em index.js
 
-app.listen(3000); // inicia o servidor na porta 5000, neste caso a API será acessivel em http://localhost (local)
+app.listen(5000); // inicia o servidor na porta 5000, neste caso a API será acessivel em http://localhost (local)
